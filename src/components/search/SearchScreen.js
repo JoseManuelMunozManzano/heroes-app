668d00383f3707ee2d3edb335bcc2572660ade05
Neimerc <Neil_Mercury@yahoo.es>
2021-01-02 20:39:56 +0100

    Aplicar filtro en base a QueryString

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,40 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 
 import { useForm } from '../../hooks/useForm';
 import { HeroCard } from '../heroes/HeroCard';
-import { heroes } from '../../data/heroes';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
 
 export const SearchScreen = ({ history }) => {
-  // Leyendo el query
-  // Ir al Chrome Developer Tools, pestaña Components de React y buscar el componente
-  // SearchScreen. Se puede ver el prop location y dentro la key search.
-  // De ahí se puede obtener
-  // En concreto se obtiene el useLocation
-  // NOTAS: Si no hay query aparece con string vacío (search: "")
-  //        Si hubiera más de un queyString habríá que parsearlo (?q=Batman&casa=DC)
-  //        Como esto es complicado se usa un paquete para trabajar con el queryString.
-  //             https://www.npmjs.com/package/query-string
-  //        Se usa mucho porque no hay manera directa para hacer esto actualmente.
   const location = useLocation();
   const { q = '' } = queryString.parse(location.search);
 
-  // Si refresco el navegador y hay algo escrito en el queryString aparecerá escrito
-  // automáticamente en el input
   const [formValues, handleInputChange] = useForm({ searchText: q });
   const { searchText } = formValues;
 
-  const heroesFiltered = heroes;
+  // Unicamente cuando presiono Enter y el String del input cambie quiero
+  // que se dispare la búsqueda
+  //const heroesFiltered = getHeroesByName(searchText);
+  const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
 
   const handleSearch = e => {
     e.preventDefault();
 
-    // Se quiere aplicar un filtro por el URL.
-    // Cuando el URL cambie se usará un queryString para aplicarlo en la página.
-    // Eso me va a mantener el history.
-    // Ejemplo de queryString: https://www.npmjs.com/search?q=react
-    // La q que aparece tras la interrogación es de query y aparece la información a buscar.
     history.push(`?q=${searchText}`);
   };
 
@@ -61,7 +47,7 @@ export const SearchScreen = ({ history }) => {
 
             <button
               type="submit"
-              className="btn m-1 btn-block btn-outline-primary"
+              className="btn mt-1 btn-block btn-outline-primary"
             >
               Search...
             </button>
@@ -71,6 +57,11 @@ export const SearchScreen = ({ history }) => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
+
+          {q === '' && <div className="alert alert-info">Search a hero</div>}
+          {q !== '' && heroesFiltered.length === 0 && (
+            <div className="alert alert-danger">There is no hero with {q}</div>
+          )}
 
           {heroesFiltered.map(hero => (
             <HeroCard key={hero.id} {...hero} />
