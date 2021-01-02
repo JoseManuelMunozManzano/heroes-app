f006ff784aa7966c05375aad5244f6a257a6b043
Neimerc <Neil_Mercury@yahoo.es>
2021-01-02 20:07:12 +0100

    Aplicar filtro de Heroes - QueryString

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,19 +1,41 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 
 import { useForm } from '../../hooks/useForm';
 import { HeroCard } from '../heroes/HeroCard';
 import { heroes } from '../../data/heroes';
 
-export const SearchScreen = () => {
-  const heroesFiltered = heroes;
+export const SearchScreen = ({ history }) => {
+  // Leyendo el query
+  // Ir al Chrome Developer Tools, pestaña Components de React y buscar el componente
+  // SearchScreen. Se puede ver el prop location y dentro la key search.
+  // De ahí se puede obtener
+  // En concreto se obtiene el useLocation
+  // NOTAS: Si no hay query aparece con string vacío (search: "")
+  //        Si hubiera más de un queyString habríá que parsearlo (?q=Batman&casa=DC)
+  //        Como esto es complicado se usa un paquete para trabajar con el queryString.
+  //             https://www.npmjs.com/package/query-string
+  //        Se usa mucho porque no hay manera directa para hacer esto actualmente.
+  const location = useLocation();
+  const { q = '' } = queryString.parse(location.search);
 
-  const [formValues, handleInputChange] = useForm({ searchText: '' });
+  // Si refresco el navegador y hay algo escrito en el queryString aparecerá escrito
+  // automáticamente en el input
+  const [formValues, handleInputChange] = useForm({ searchText: q });
   const { searchText } = formValues;
+
+  const heroesFiltered = heroes;
 
   const handleSearch = e => {
     e.preventDefault();
 
-    console.log(searchText);
+    // Se quiere aplicar un filtro por el URL.
+    // Cuando el URL cambie se usará un queryString para aplicarlo en la página.
+    // Eso me va a mantener el history.
+    // Ejemplo de queryString: https://www.npmjs.com/search?q=react
+    // La q que aparece tras la interrogación es de query y aparece la información a buscar.
+    history.push(`?q=${searchText}`);
   };
 
   return (
